875d1e68e0c6425ee4818196274996984ade8f2f
Kanishkaudara19 <kanishkaudara2000@gmail.com>
2025-04-27 23:17:49 +0530

    create route groups

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,18 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 //Posts
-Route::post('/posts/store',[PostController::class,'store'])->name('posts.store');
 Route::get('/posts/{postId}/show',[PostController::class,'show'])->name('posts.show');
-Route::get('/posts/all',[HomeController::class,'allPost'])->name('posts.all');
-Route::get('/posts/{postId}/edit',[PostController::class,'edit'])->name('posts.edit');
-Route::post('/posts/{postId}/update',[PostController::class,'update'])->name('posts.update');
-Route::get('/posts/{postId}/delete',[PostController::class,'delete'])->name('posts.delete');
+Route::group(['middleware'=>'auth'],function (){
+    Route::post('/posts/store',[PostController::class,'store'])->name('posts.store');
+    Route::get('/posts/all',[HomeController::class,'allPost'])->name('posts.all');
+    Route::get('/posts/{postId}/edit',[PostController::class,'edit'])->name('posts.edit');
+    Route::post('/posts/{postId}/update',[PostController::class,'update'])->name('posts.update');
+    Route::get('/posts/{postId}/delete',[PostController::class,'delete'])->name('posts.delete');
+});
 
 //Admin Routes
-Route::get('/admin/dashboard',[DashboardController::class,'index'])->middleware('admin')->name('admin.dashboard');
+Route::group(['middleware'=>['admin','auth'],'prefix'=>'admin','as'=>'admin.'],function (){
+    Route::get('dashboard',[DashboardController::class,'index'])->middleware('admin')->name('dashboard');
+});
+
+
